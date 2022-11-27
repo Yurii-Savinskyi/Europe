f8124b4109e0dda42350c5846d80d29731c2fe47
Intell\ASUS <yur.savinskij@gmail.com>
2022-11-27 19:03:41 +0200

    1st

--- Europe/Europe/Program.cs
+++ Europe/Europe/Program.cs
@@ -9,6 +9,7 @@ namespace Europe
             Console.WriteLine("West Europe is composed of:");
             Console.WriteLine("9 countries, such as:");
             Console.WriteLine("Germany, France, Austria");
+            Console.WriteLine("Germany:");
         }
     }
 }
